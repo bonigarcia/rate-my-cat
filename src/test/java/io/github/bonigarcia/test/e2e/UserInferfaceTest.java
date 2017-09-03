28756003c1b3b1fc70b6eed7baf1818ca937684f
Boni Garcia <bgarcia@gsyc.es>
2017-09-03 20:23:37 +0200

    Add e2e test using selenium-jupiter

---
+++ src/test/java/io/github/bonigarcia/test/e2e/UserInferfaceTest.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.test.e2e;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import io.github.bonigarcia.SeleniumExtension;
+
+@DisplayName("Test of the web user interface")
+@ExtendWith({ SpringExtension.class, SeleniumExtension.class })
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class UserInferfaceTest {
+
+    @LocalServerPort
+    int serverPort;
+
+    @Test
+    @DisplayName("Browser title test")
+    public void testTitle(ChromeDriver chrome) {
+        chrome.get("http://localhost:" + serverPort);
+        assertThat(chrome.getTitle(), equalTo("Rate my cat!"));
+    }
+
+}
