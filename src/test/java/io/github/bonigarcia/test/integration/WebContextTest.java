e58546521d4c77e6c27c2df9e39218841a2a7113
Boni Garcia <bgarcia@gsyc.es>
2017-09-16 21:25:39 +0200

    Using @AutoConfigureMockMvc for MockMcv in spring-boot-web project

--- src/test/java/io/github/bonigarcia/test/integration/WebContextTest.java
+++ src/test/java/io/github/bonigarcia/test/integration/WebContextTest.java
@@ -21,20 +21,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 @DisplayName("Integration tests: HTTP reponses")
 @Tag("integration")
 @Tag("functional-requirement-1")
@@ -42,14 +41,7 @@ import org.springframework.web.context.WebApplicationContext;
 class WebContextTest {
 
     @Autowired
-    WebApplicationContext webContext;
-
     MockMvc mockMvc;
-
-    @BeforeEach
-    void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
-    }
 
     @Test
     @DisplayName("Check home page (GET /)")
